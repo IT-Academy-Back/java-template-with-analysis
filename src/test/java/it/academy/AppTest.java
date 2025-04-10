519d027aaf26f26056f8ea92fff09cae82dad6bb
Sergi Virgili <svirgilif@gmail.com>
2025-04-10 13:15:57 +0200

    Revert "test: try action page coverage"
    This reverts commit a7d117b95f7cb29f5e9222001e6e7b8e2a55618b.

--- src/test/java/it/academy/AppTest.java
+++ src/test/java/it/academy/AppTest.java
@@ -8,6 +8,6 @@ public class AppTest {
     @Test
     public void appShouldRun() {
         System.out.println("Application is running...");
-        assertTrue(false, "This test should fail");
+        assertTrue(true);
     }
 }
